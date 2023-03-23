0693afefbac705b591dce38d35195e6f6c826097
abdullo69 <abdulloakbarov28@gmail.com>
2023-03-23 14:25:18 +0500

    aeff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Ins1 from './components/Ins1/Ins1';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Ins1 />
     </div>
   );
 }
