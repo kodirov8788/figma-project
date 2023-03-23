de2027cc32335d468fbfb8a3648ce5111e14acbc
kodirov8788 <kodirov8788@gmail.com>
2023-03-23 14:58:34 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Ins1 from './components/Ins1/Ins1';
+import Navbar from "./components/navbar/Navbar"
+import Main from "./components/main/Main"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Main />
       <Ins1 />
     </div>
   );
